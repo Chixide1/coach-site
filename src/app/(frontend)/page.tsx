062cfe30a045672409add451b19eb9e7617a729e
Chik <chixide12@gmail.com>
2024-09-08 01:58:48 +0100

    style: - created mockup for services

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -7,6 +7,7 @@ export default async function Homepage(){
   <main>
     <Intro/>
     <About/>
+    <Services/>
   </main>
   )
 }
--- src/components/services.tsx
+++ src/components/services.tsx
@@ -1,8 +1,14 @@
 import { getPayloadHMR } from "@payloadcms/next/utilities"
 import config from '../payload.config'
+import Heading from "./heading"
+import { Playfair_Display } from 'next/font/google'
 
-export default async function Services(){
-  const payload = await getPayloadHMR({config})
+const pdFont = Playfair_Display({
+  subsets: ['latin']
+})
+
+export default async function Services() {
+  const payload = await getPayloadHMR({ config })
 
   const services = await payload.findByID({
     collection: 'pages',
@@ -13,14 +19,21 @@ export default async function Services(){
     )
   )
 
+  services?.map(service => {
+    service.content_html = service.content_html?.replace(new RegExp('<h2>', 'g'), `<h2 class=${pdFont.className}>`);
+  });
+
   return (
-    <section className="min-h-screen bg-white px-20 py-28">
-      <div className="flex flex-col gap-4 bg-primary rounded-lg p-9 w-fit">
-        {services?.map(service => (
-          <hgroup dangerouslySetInnerHTML={{__html: service.content_html || ''}}
-          className="bg-white [&>h2]:text-2xl [&>h2]:text-secondary [&>p]:text-accent   rounded-lg p-5 border-accent/30 border max-w-96 shadow-lg"/>
-        ))}
-      </div>
-    </section>
+    <div className="bg-accent/5 pt-8">
+      <Heading heading="Services Provided" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, consequuntur?"/>
+      <section className="min-h-screen px-20 py-6 flex gap-4">
+          {services?.map(service => (
+            <hgroup
+              dangerouslySetInnerHTML={{ __html: service.content_html || '' }}
+              className={`rounded-lg p-8 flex flex-col items-center bg-white [&>h2]:text-xl [&>h2]:font-semibold [&>h2]:text-secondary h-fit`}
+            />
+          ))}
+      </section>
+    </div>
   )
 }
